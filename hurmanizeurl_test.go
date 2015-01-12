0612f026d3d0d3be044a1e50c1ad7338663e5518
Masayoshi Sekimura <sekimura@gmail.com>
2015-01-12 18:41:43 +0000

    chore: tweaking test data with anon struct

--- hurmanizeurl_test.go
+++ hurmanizeurl_test.go
@@ -6,26 +6,28 @@ import (
 
 func TestHumanize(t *testing.T) {
 
-	var tests = map[string]string{
-		"http://sekimura.org":      "sekimura.org",
-		" http://sekimura.org":     "sekimura.org",
-		"https://sekimura.org":     "sekimura.org",
-		"http://www.sekimura.org":  "sekimura.org",
-		"www.sekimura.org":         "sekimura.org",
-		"http://sekimura.org/":     "sekimura.org",
-		"sekimura.org/":            "sekimura.org",
-		"http://sekimura.org/foo/": "sekimura.org/foo",
-
-		"file:///Users/sindresorhus/dev/humanize-url/": "file:///Users/sindresorhus/dev/humanize-url",
+	tests := []struct {
+		value    string
+		expected string
+	}{
+		{"http://sekimura.org", "sekimura.org"},
+		{" http://sekimura.org", "sekimura.org"},
+		{"https://sekimura.org", "sekimura.org"},
+		{"http://www.sekimura.org", "sekimura.org"},
+		{"www.sekimura.org", "sekimura.org"},
+		{"http://sekimura.org/", "sekimura.org"},
+		{"sekimura.org/", "sekimura.org"},
+		{"http://sekimura.org/foo/", "sekimura.org/foo"},
+		{"file:///Users/sindresorhus/dev/humanize-url/", "file:///Users/sindresorhus/dev/humanize-url"},
 	}
 
-	for orig, expected := range tests {
-		actual, err := humanize(orig)
+	for i := range tests {
+		actual, err := humanize(tests[i].value)
 		if err != nil {
-			t.Error("humanize failed", err, expected)
+			t.Error("humanize failed", err)
 		}
-		if actual != expected {
-			t.Error("humanize did not match expeted value", actual, expected)
+		if actual != tests[i].expected {
+			t.Error("humanize did not match expected value", actual, tests[i].expected)
 		}
 	}
 }
